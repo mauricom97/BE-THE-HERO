f35a65f6c5f1e12a4ab40348b290d14ffe86b0da
Mauricio <mauricom7@hotmail.com>
2020-03-26 19:02:39 -0300

    Front-end da aplicação

---
+++ frontend/src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react;'
+import { BrowserRouter} from 'react-router-dom';
+
+import Logon from './pages/Logon';
+
+
+export default function Routes(){
+    return(
+        <BrowserRouter></BrowserRouter>
+    )
+}
+
